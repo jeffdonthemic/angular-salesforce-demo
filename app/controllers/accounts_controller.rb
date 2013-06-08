0e0108c2cfa251414b403e90da5a3b28bfda152c
Jeff Douglas <jeff@jeffdouglas.com>
2013-06-07 20:31:31 -0400

    final ui tweaks

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,39 +2,47 @@ class AccountsController < ApplicationController
   respond_to :json
 
   def index
-    puts "running index"
-    respond_with client.query('select id, name, type from account limit 10')
+    # simply return the query as json
+    respond_with client.query('select id, name, type, billingstate from account 
+      order by lastmodifieddate desc limit 5')
   end
 
   def show
-    respond_with client.query("select id, name, type from account where id = '#{params[:id]}'").first
+    # return the requested record as json
+    respond_with client.query("select id, name, type, billingstate from account 
+      where id = '#{params[:id]}'").first
   end	
 
-  # curl -v  -H "Content-type: application/json" -X POST -d '{"name": "some test name"}' http://localhost:3000/accounts
+  # create a new record in salesforce. Use the following command for testing:
+  #   curl -v  -H "Content-type: application/json" -X POST -d '{"name": "some test name"}' http://localhost:3000/accounts
   def create
-    # create the account in salesforce
-    id = client.create!('Account', Name: params[:name])
-    # query for the newly created resource
+    # create the account in salesforce and get the new id
+    id = client.create!('Account', Name: params[:name], Type: 'Prospect', BillingState: 'NY')
+    # query for the newly created account
     account = client.query("select id, name, type from account where id = '#{id}'").first
     # return the resource so it can be added to the collection
     respond_with(account, :status => :created, :location => account_url(account))
   end		
 
   def update
-    client.update!('Account', Id: params[:account][:Id], Name: params[:account][:Name], Type: params[:account][:Type])
-    respond_with client.query("select id, name from account where id ='#{params[:account][:Id]}'")
+    # update only two of the fields in salesforce and return the record
+    client.update!('Account', Id: params[:account][:Id], Name: params[:account][:Name], 
+      Type: params[:account][:Type])
+    respond_with client.query("select id, name, type, billingstate from account 
+      where id ='#{params[:account][:Id]}'")
   end		
 
-	private
+  private
 
-        def client
-          client = Restforce.new :username => ENV['SFDC_USERNAME'],
-            :password       => ENV['SFDC_PASSWORD'],
-            :client_id      => ENV['SFDC_CLIENT_ID'],
-            :client_secret  => ENV['SFDC_CLIENT_SECRET'],
-            :host           => ENV['SFDC_HOST']
-          client.authenticate!
-          client
-        end
+    # define the restforce client and authenticate
+    def client
+      client = Restforce.new :username => ENV['SFDC_USERNAME'],
+        :password       => ENV['SFDC_PASSWORD'],
+        :client_id      => ENV['SFDC_CLIENT_ID'],
+        :client_secret  => ENV['SFDC_CLIENT_SECRET'],
+        :host           => ENV['SFDC_HOST']
+      client.authenticate!
+      client
+    end
 
 end
