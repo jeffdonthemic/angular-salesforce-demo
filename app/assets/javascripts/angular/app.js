8c9353afaebf077f4a6409072c2fc67fd5188ac2
Jeff Douglas <jeff@jeffdouglas.com>
2013-06-09 16:16:59 -0400

    reformatted the code

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -5,14 +5,17 @@
     the routing information.
 
     Our module will be called 'app'.
-
-    Note: We need the empty array after the 'app' declaration, without it, you
-    get an error complaining about 'No module <MODULE>'.
  */
 angular.module('app', ['ngResource'])
-    .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-        $routeProvider
-            .when('/', {controller: 'AccountListCtrl', templateUrl: '/assets/angular/templates/index.html'})
-            .when('/accounts/:id', {controller: 'AccountDetailCtrl', templateUrl: '/assets/angular/templates/details.html'})
-            .otherwise({redirectTo: '/'});
+  .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        controller: 'AccountListCtrl', 
+        templateUrl: '/assets/angular/templates/index.html'
+      })
+      .when('/accounts/:id', {
+        controller: 'AccountDetailCtrl', 
+        templateUrl: '/assets/angular/templates/details.html'
+      })
+      .otherwise({redirectTo: '/'});
 }]);
